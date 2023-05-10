5e11acc03767f96959ae966a77d4f6d4df67987e
Oskina Margarita <rita_2113@mail.ru>
2023-05-10 19:32:36 +0300

    Done

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,14 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Double percent = 9.99;
+        int sumCredit = 1000000;
+        CreditPaymentService service = new CreditPaymentService();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        System.out.println("Ежемесечный платёж за 1 год: " + service.calculate(1, sumCredit, percent));
+        System.out.println("Ежемесечный платёж за 2 год: " + service.calculate(2, sumCredit, percent));
+        System.out.println("Ежемесечный платёж за 3 год: " + service.calculate(3, sumCredit, percent));
+
     }
 }
